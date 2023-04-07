fbcd2852d7b1fc61b5dfd0a4c8b441a873594cc5
Dré Van den Hooff <dre.vandenhooff@student.hogent.be>
2023-04-07 17:09:24 +0200

    pages use page component

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,14 +1,10 @@
-import { useNavigate } from 'react-router';
+import Page from '../../common/page';
 
 const Home = () => {
-  const navigate = useNavigate();
   return (
-    <div>
+    <Page>
       <p>This is the home page</p>
-      <button type="button" onClick={() => navigate('/settings')}>
-        To settings
-      </button>
-    </div>
+    </Page>
   );
 };
 
