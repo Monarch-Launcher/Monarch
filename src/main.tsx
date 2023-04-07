2aa154edbc65d456ca9da508d64cb15db865ba17
Dré Van den Hooff <dre.vandenhooff@student.hogent.be>
2023-04-08 00:38:07 +0200

    min window size

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { appWindow, LogicalSize } from '@tauri-apps/api/window';
 import App from './App';
 import './styles.css';
+
+await appWindow.setMinSize(new LogicalSize(800, 500));
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
