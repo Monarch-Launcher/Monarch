184123521c90151a914419c470fbbc2b71936866
Dré Van den Hooff <dre.vandenhooff@student.hogent.be>
2023-04-22 17:31:44 +0200

    fix typo issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { appWindow, LogicalSize } from '@tauri-apps/api/window';
-import App from './app';
+import App from './App';
 import './styles.css';
 
 // Set minimum size of window
