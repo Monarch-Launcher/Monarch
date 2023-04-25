a4329be671754b35830a33b8ffc3aeaa518981ba
an0nymoos3 <moosely123@gmail.com>
2023-04-25 22:21:34 +0200

    added blizzard games to search

--- src-tauri/src/monarch_games/blizzard.rs
+++ src-tauri/src/monarch_games/blizzard.rs
@@ -1,18 +1,23 @@
 use log::{info, error};
 use std::io::Error;
 use std::process::{Command, Child};
+use std::collections::HashMap;
 
 use crate::monarch_utils::{monarch_winreg::is_installed, monarch_download::download_and_run};
+use crate::monarch_utils::monarch_fs::{generate_cache_image_name, generate_library_image_name, path_exists};
+use crate::monarch_utils::monarch_download::download_image;
+use super::monarchgame::MonarchGame;
+
 /*
 This is hopefully not a long time solution. For now running battlenet://<game> only opens battlenet page and doesn't run game.
 Here are game codes:
-    Hearthstone: WTCG
+    Destiny 2: DST2
     Diablo 3: D3
+    Hearthstone: WTCG
+    Heroes of the Storm: Hero
+    Overwatch2: Pro
     Starcraft 2: SC2
     World of Warcraft: WoW
-    Heroes of the Storm: Hero
-    Overwatch: Pro
-    Destiny 2: DST2
 */
 
 /*
@@ -51,6 +56,23 @@ pub fn launch_game(name: &str, id: &str) {
     }
 }
 
+/// Attempt to find blizzard game matching search term
+pub fn find_game(name: &str) -> Vec<MonarchGame> {
+    let known_games: [&str; 7] = ["Destiny 2", "Diablo 3", "Hearthstone", "Heroes of the Storm", "Overwatch 2", "Starcraft 2", "World of Warcraft"];
+    let mut games: Vec<MonarchGame> = Vec::new();
+
+    for game_name in known_games {
+        let lower_name = game_name.to_lowercase().replace(" ", "");
+        
+        if lower_name.contains(name) {
+            let game: MonarchGame = get_blizz_game(&lower_name, false);
+            games.push(game)
+        }
+    }
+
+    return games
+}
+
 /*
 ----------- Private Blizzard related function -----------
 */
@@ -58,4 +80,48 @@ pub fn launch_game(name: &str, id: &str) {
 /// Sepcifically checks if Battle.net is installed
 fn blizzard_is_installed() -> bool {
     return is_installed(r"Blizzard Entertainment\Battle.net");
+}
+
+/// Creates and returns Blizzard owned MonarchGame
+fn get_blizz_game(name: &str, is_library: bool) -> MonarchGame {
+    let path: String;
+    let names_and_ids: HashMap<&str, &str> = HashMap::from([
+        ("destiny2", "DST2"),
+        ("diablo3", "D3"),
+        ("hearthstone", "WTCG"),
+        ("heroesofthestorm", "Hero"),
+        ("overwatch2", "Pro"),
+        ("starcraft2", "SC2"),
+        ("worldofwarcraft", "WoW")
+    ]);
+
+    let names_and_links: HashMap<&str, &str> = HashMap::from([
+        ("destiny2", "https://images.contentstack.io/v3/assets/blte410e3b15535c144/blt8599cdc8468fb924/630fd93c2d08277c7e733f1e/hero_bg_desktop.jpg"),
+        ("diablo3", "https://wallpaperaccess.com/full/7471248.jpg"),
+        ("hearthstone", "https://wallpaperaccess.com/full/7471195.jpg"),
+        ("heroesofthestorm", "https://wallpaperaccess.com/full/7471312.jpg"),
+        ("overwatch2", "https://wallpaperaccess.com/full/7471219.jpg"),
+        ("starcraft2", "https://wallpaperaccess.com/full/7471222.jpg"),
+        ("worldofwarcraft", "https://wallpaperaccess.com/full/1692125.jpg")
+    ]);
+
+    if is_library {
+        path = generate_library_image_name(name);
+    } else {
+        path = generate_cache_image_name(name);
+    }
+
+    let link = names_and_links.get(name).unwrap().clone();
+    let send_path = path.clone();
+
+    println!("{}, {}", link, send_path);
+
+    if !path_exists(&path) { // Only download if image is not in cache dir
+        // Workaround for [tauri::command] not working with download_image().await in same thread 
+        tokio::task::spawn(async move {
+            download_image(link, &send_path).await; 
+        });
+    }
+
+    MonarchGame::new(name, "blizzard", names_and_ids.get(name).unwrap(), "temp", &path)
 }
