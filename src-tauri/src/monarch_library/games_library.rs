6b82c14584cb007e0fca0a3f813f4d815cfd26e8
an0nymoos3 <moosely123@gmail.com>
2023-12-04 23:25:54 +0100

    initial anyhow implementation of monach_library

--- src-tauri/src/monarch_library/games_library.rs
+++ src-tauri/src/monarch_library/games_library.rs
@@ -1,171 +1,85 @@
 use log::error;
 use std::fs;
+use std::fs::File;
 use std::path::PathBuf;
 use serde_json::{value::Value, json};
+use anyhow::{Context, Result, anyhow};
 
 use crate::monarch_games::monarchgame::MonarchGame;
 use crate::monarch_utils::monarch_fs::{write_json_content, get_library_json_path, get_monarch_games_path, path_exists};
 
-pub fn write_games(games: Vec<MonarchGame>) -> Result<(), String> {
-    let path: PathBuf;
+pub fn write_games(games: Vec<MonarchGame>) -> Result<()> {
+    let path: PathBuf = get_library_json_path().with_context(||
+        -> String {format!("games_library::write_games() failed! Cannot get path to library.json! | Err")})?;
 
-    match get_library_json_path() {
-        Ok(json_path) => { path = json_path; }
-        Err(e) => {
-            error!("games_library::write_games() failed! Cannot get path to library.json! | Error: {e}");
-            return Err("Failed to get path to library.json!".to_string())
-        } 
-    }
-
-    if let Err(e) = write_json_content(json!(games), &path) {
-        error!("games_library::write_games() failed! Error while writing library to: {file} | Error: {e}", file = path.display());
-        return Err("Failed to write content to library.json!".to_string())
-    }
-
-    Ok(())
+    write_json_content(json!(games), &path)
+        .context(format!("games_library::write_games() failed! Error while writing library to: {file} | Err", file = path.display()))
 }
 
 /// Writes new games to monarch_games.json for Monarch to track what games it installed itself.
-pub fn write_monarchgame(game: MonarchGame) -> Result<(), String> {
-    let path: PathBuf;
+pub fn write_monarchgame(game: MonarchGame) -> Result<()> {
+    let path: PathBuf = get_monarch_games_path().with_context(||
+        -> String {format!("games_library::write_monarchgame() failed! Cannot get path to monarch_games.json! | Err")})?;
+    
     let mut games: Vec<MonarchGame> = Vec::new();
-
-    match get_monarch_games_path() {
-        Ok(json_path) => { path = json_path; }
-        Err(e) => {
-            error!("games_library::write_monarchgame() failed! Cannot get path to library.json! | Error: {e}");
-            return Err("Failed to get path to library.json!".to_string())
-        } 
-    }
 
     if !path_exists(&path) {
         if let Err(e) = fs::File::create(&path) {
             error!("games_library::get_monarchgames() failed! Could not create new file {file} | Error: {e}", file = path.display());
-            return Err(String::from("Failed to create new monarch_games.json!"))
+            return Err(anyhow!("Failed to create new monarch_games.json!"))
         }
     }
     else {
-        match fs::File::open(&path) {
-            Ok(file) => {
-                match serde_json::from_reader::<fs::File, Vec<MonarchGame>>(file) {
-                    Ok(content) => { 
-                        games = content;
-                    }
-                    Err(e) => {
-                        error!("games_library::monarchget_games() failed! Failed to parse json! | Error: {e}");
-                        return Err("Failed to parse library.json content!".to_string())
-                    }
-                }
-            }
-            Err(e) => {
-                error!("games_library::get_monarchgames() failed! Error opening: {file} | Error: {e}", file = path.display());
-                return Err("Failed to open library.json!".to_string())
-            }
+        let file: File = fs::File::open(&path).with_context(||
+            -> String {format!("games_library::get_monarchgames() failed! Error opening: {file} | Err", file = path.display())})?;
+        
+        if let Ok(content) = serde_json::from_reader::<fs::File, Vec<MonarchGame>>(file) {
+            games = content;
         }
     }
 
     games.push(game);
-    if let Err(e) = write_json_content(json!(games), &path) {
-        error!("games_library::write_monarchgame() failed! Error while writing library to: {file} | Error: {e}", file = path.display());
-        return Err("Failed to write content to library.json!".to_string())
-    }
-
-    Ok(())
+    write_json_content(json!(games), &path).context(format!("games_library::write_monarchgame() failed! Error while writing library to: {file} | Err", file = path.display()))
 }
 
 /// Returns JSON of games from library
-pub fn get_games() -> Result<Value, String> {
-    let games: Value;
-    let path: PathBuf;
+pub fn get_games() -> Result<Value> {
+    let path: PathBuf = get_library_json_path().with_context(|| 
+    -> String {format!("games_library::get_games() failed! Cannot get path to library.json! | Err")})?;
 
-    match get_library_json_path() {
-        Ok(json_path) => { path = json_path; }
-        Err(e) => {
-            error!("games_library::get_games() failed! Cannot get path to library.json! | Error: {e}");
-            return Err("Failed to get path to library.json!".to_string())
-        }
-    }
+    let file: File = fs::File::open(&path).with_context(|| 
+        -> String {format!("games_library::get_games() failed! Error opening: {file} | Err", file = path.display())})?;
 
-    match fs::File::open(&path) {
-        Ok(file) => {
-            match serde_json::from_reader(file) {
-                Ok(json_content) => { games = json_content }
-                Err(e) => {
-                    error!("games_library::get_games() failed! Failed to parse json! | Error: {e}");
-                    return Err("Failed to parse library.json content!".to_string())
-                }
-            }
-        }
-        Err(e) => {
-            error!("games_library::get_games() failed! Error opening: {file} | Error: {e}", file = path.display());
-            return Err("Failed to open library.json!".to_string())
-        }
-    }
-    return Ok(games)
+    let games: Value = serde_json::from_reader(file).with_context(||
+        -> String {format!("games_library::get_games() failed! Failed to parse json! | Err")})?;
+    
+    return Ok(games) // Seperate return statement for verbosity
 }
 
 /// Returns JSON of games installed by Monarch
-pub fn get_monarchgames() -> Result<Vec<MonarchGame>, String> {
-    let games: Vec<MonarchGame>;
-    let path: PathBuf;
+pub fn get_monarchgames() -> Result<Vec<MonarchGame>> {
+    let path: PathBuf = get_monarch_games_path().with_context(|| 
+        -> String {format!("games_library::get_monarchgames() failed! Cannot get path to library.json! | Err")})?;
+    
+    let file: File = fs::File::open(&path).with_context(|| 
+        -> String {format!("games_library::get_monarchgames() failed! Error opening: {file} | Err", file = path.display())})?;
 
-    match get_monarch_games_path() {
-        Ok(json_path) => { path = json_path; }
-        Err(e) => {
-            error!("games_library::get_monarchgames() failed! Cannot get path to library.json! | Error: {e}");
-            return Err("Failed to get path to library.json!".to_string())
-        }
-    }
-    match fs::File::open(&path) {
-        Ok(file) => {
-            match serde_json::from_reader(file) {
-                Ok(json_content) => { 
-                    match serde_json::from_value::<Vec<MonarchGame>>(json_content) {
-                        Ok(content) => {
-                            return Ok(content)
-                        }
-                        Err(e) => {
-                            error!("games_library::get_monarchgames() failed! Could not parse json value as Vec<MonarchGame> | Error: {e}");
-                            return Err(String::from("Failed to parse json to Vec<MonarGame>"))
-                        }
-                    }
-                }
-                Err(e) => {
-                    error!("games_library::monarchget_games() failed! Failed to parse json! | Error: {e}");
-                    return Err("Failed to parse library.json content!".to_string())
-                }
-            }
-        }
-        Err(e) => {
-            error!("games_library::get_monarchgames() failed! Error opening: {file} | Error: {e}", file = path.display());
-            return Err("Failed to open library.json!".to_string())
-        }
-    }
+    let games: Vec<MonarchGame> = serde_json::from_reader(file).with_context(|| 
+        -> String {format!("games_library::get_monarchgames() failed! Could not parse json value as Vec<MonarchGame> | Er")})?;
+
     return Ok(games)
 }
 
 /// Backend functionality for adding a new game that's been installed.
-pub fn add_game(game: MonarchGame) -> Result<(), String> {
-    if let Err(e) = write_monarchgame(game.clone()) {
-        error!("games_library::add_game() failed! Could not write new game to monarch_games.json! | Error: {e}");
-        return Err(String::from("Could not write new game to monarch_games.json!"))
-    }
-    match get_games() {
-        Ok(games_json) => {
-            match serde_json::from_value::<Vec<MonarchGame>>(games_json) {
-                Ok(mut vec) => {
-                    vec.push(game);
-                    return write_games(vec)
-                }
-                Err(e) => {
-                    error!("games_library::add_game() failed! Failed to parse json to Vec<MonarchGame>! | Error: {e}");
-                    return Err(String::from("Failed to add new game!"))
-                }
-            }
-        }
-        Err(e) => {
-            error!("games_library::add_game() failed! get_games() returned errro! | Error: {e}");
-            return Err("Failed to add new game!".to_string())
-        }
-    }
+pub fn add_game(game: MonarchGame) -> Result<()> {
+    write_monarchgame(game.clone()).context(format!("games_library::add_game() failed! Could not write new game to monarch_games.json! | Err"));
+
+    let games_json: Value = get_games().with_context(|| 
+        -> String {format!("games_library::add_game() failed! get_games() returned error! | Err")})?;
+
+    let games: Vec<MonarchGame> = serde_json::from_value(games_json).with_context(|| 
+        -> String {format!("games_library::add_game() failed! Failed to parse json to Vec<MonarchGame>! | Err")})?;
+
+    games.push(game);
+    write_games(games)
 }
