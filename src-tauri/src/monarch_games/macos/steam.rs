89823a7e5d95949bea0732b0bd6cb1f4f64476b5
an0nymoos3 <moosely123@gmail.com>
2025-08-06 12:55:05 +0200

    macos compiles, but doesn't do more than launch

--- src-tauri/src/monarch_games/macos/steam.rs
+++ src-tauri/src/monarch_games/macos/steam.rs
@@ -2,6 +2,7 @@ use crate::monarch_games::monarchgame::MonarchGame;
 use anyhow::{bail, Result};
 use tauri::AppHandle;
 use tracing::error;
+use std::path::PathBuf;
 
 /*
 * This file is currently only here to allow for MacOS compilation.
@@ -31,4 +32,16 @@ pub async fn get_library() -> Vec<MonarchGame> {
 pub fn run_command(args: &str) -> Result<()> {
     error!("monarch_games::macos::run_command() MacOS not currently supported!");
     bail!("monarch_games::macos::run_command() MacOS not currently supported!");
+}
+
+/// Returns default path used by steam on Linux systems ($HOME/.steam)
+pub fn get_default_location() -> Result<PathBuf> {
+    error!("monarch_games::macos::get_default_location() MacOS not currently supported!");
+    bail!("monarch_games::macos::get_default_location() MacOS not currently supported!");
+}
+
+/// Returns default path to libraryfolders.vdf used by steam on Linux systems
+pub fn get_default_libraryfolders_location() -> Result<PathBuf> {
+    error!("monarch_games::macos::get_libraryfolders_location() MacOS not currently supported!");
+    bail!("monarch_games::macos::get_libraryfolders_location() MacOS not currently supported!");
 }
