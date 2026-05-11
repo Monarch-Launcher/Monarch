7d41377e442afc11b15e2bce4802d3f68840d84c
an0nymoos3 <moosely123@gmail.com>
2026-05-11 14:58:32 +0200

    auth code login APIs work

--- crates/monarch_egs/src/auth/user.rs
+++ crates/monarch_egs/src/auth/user.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
+
 use reqwest::{Client, Response, Result};
 use serde_json::Value;
 
 use crate::auth::session::Session;
 
-static OAUTH_HOST: &str = "account-public-service-prod03.ol.epicgames.com";
+static OAUTH_HOST: &str = "https://account-public-service-prod03.ol.epicgames.com";
 static BASIC_USERNAME: &str = "34a02cf8f4414e29b15921876da36f9a";
 static BASIC_PASSWORD: &str = "daafbccc737745039dffe53d94fc76cf";
 
@@ -22,7 +24,7 @@ impl User {
         let url: &str = std::env!("EPICLOGIN_URL");
         #[cfg(target_os = "linux")]
         {
-            let _ = std::process::Command::new("xdg-open").arg(&url).spawn();
+            let _ = std::process::Command::new("xdg-open").arg(url).spawn();
         }
         #[cfg(target_os = "windows")]
         {
@@ -39,35 +41,37 @@ impl User {
     }
 
     async fn login_with_auth_code(&mut self, auth_code: &str) -> Result<()> {
-        let body: String = format!(
-            r#"{{"grant_type": "authorization_code", "code": "{}", "token_type": "eg1"}}"#,
-            auth_code
-        );
+        let form: HashMap<&str, &str> = HashMap::from([
+            ("grant_type", "authorization_code"),
+            ("code", auth_code),
+            ("token_type", "eg1"),
+        ]);
+
+        let url: String = format!("{}/account/api/oauth/token", OAUTH_HOST);
 
         let client: Client = Client::new();
         let response: Response = client
-            .post(OAUTH_HOST)
+            .post(url)
+            .form(&form)
             .basic_auth(BASIC_USERNAME, Some(BASIC_PASSWORD))
-            .body(body)
             .send()
             .await?;
 
         if response.status().is_server_error() {
             // TODO: Do something
+            panic!("http 5XX")
         } else if response.status().is_client_error() {
             // TODO: Do something
+            panic!("http 4XX")
         } else if !response.status().is_success() {
             // TODO: Do something
+            panic!("not 2XX")
         }
 
         let response_text: String = response.text().await?;
         let response_json: Value = serde_json::from_str(&response_text).unwrap();
         let token: String = response_json["access_token"].to_string();
         self.session.set_access_token(token);
-
-        println!("{:?}", response_text);
-        println!("------------------------------------------------------");
-        println!("{:?}", response_json);
 
         Ok(())
     }
